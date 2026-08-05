e7749ad86c7de4cb0bd7994eb3bfbc4b7a08f139
rtiqa <admin@rtiqa.com>
2026-08-05 21:03:07 +0300

    build: refactor ktor-starter build configuration
    Standardize plugin management for the Ktor microservice by utilizing version catalogs and updating the CI build command to resolve module pathing issues.

--- deploy/ktor-starter/build.gradle.kts
+++ deploy/ktor-starter/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
-    alias(libs.plugins.kotlin.serialization)
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+    application
 }
 
 group = "com.rtiqa.backend"
